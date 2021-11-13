24a26e592cc9326f1b02d25992d3b2fb61bc0b75
Quentin BUXO <katouprog@gmail.com>
2021-11-13 20:43:28 +0900

    BACK - Config du routage

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const Product = require('./models/Product');
+const saucesRoutes = require('./routes/sauces')
 
 mongoose.connect('mongodb+srv://katouprog:Janjakus30@piiquanteocp6.75o3e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -21,32 +21,6 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.post('/api/sauces', (req, res, next) => {
-    delete req.body._id;
-    const product = new Product({
-      ...req.body
-    });
-    product.save()
-      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
-
-app.put('/api/sauces/:id', (req, res, next) => {
-Product.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.get('/api/sauces/:id', (req, res, next) => {
-Product.findOne({ _id: req.params.id })
-    .then(product => res.status(200).json(product))
-    .catch(error => res.status(404).json({ error }));
-});
-
-app.use('/api/sauces', (req, res, next) => {
-Product.find()
-    .then(products => res.status(200).json(products))
-    .catch(error => res.status(400).json({ error }));
-});
+app.use('./api/sauces', saucesRoutes);
 
 module.exports = app;
