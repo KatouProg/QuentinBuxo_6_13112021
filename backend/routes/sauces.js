b190954678c62a693264de2a074970d691c555f2
Quentin BUXO <katouprog@gmail.com>
2021-11-15 21:19:02 +0900

    FRONT - BACK Modifs avec mentor

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
 const saucesCtrl = require('../controllers/sauces');
 
-router.get('/', auth, saucesCtrl.getAllSauces);
-router.post('/', auth, saucesCtrl.createProduct);
-router.get('/:id', auth, saucesCtrl.getOneProduct);
-router.put('/:id', auth, saucesCtrl.modifyProduct);
+router.post('/', auth, multer, saucesCtrl.createProduct);
+router.put('/:id', auth, multer, saucesCtrl.modifyProduct);
 router.delete('/:id', auth, saucesCtrl.deleteProduct);
-//router.post('/:id/like', auth, saucesCtrl.addLike);
+router.get('/:id', auth, saucesCtrl.getOneProduct);
+router.get('/', auth, saucesCtrl.getAllSauces);
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const app = require('./app');
+const cors = require('cors');
 
 const normalizePort = val => {
   const port = parseInt(val, 10);
@@ -34,6 +35,8 @@ const errorHandler = error => {
       throw error;
   }
 };
+
+app.use(cors());
 
 const server = http.createServer(app);
 
