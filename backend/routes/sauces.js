e7c270b76b5869e38d2b0ae0c8fdf37178ddb522
Quentin BUXO <katouprog@gmail.com>
2021-11-20 17:36:51 +0900

    BACK - changement Product.js --> Sauce.js

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -6,10 +6,10 @@ const multer = require('../middleware/multer-config');
 
 const saucesCtrl = require('../controllers/sauces');
 
-router.post('/', auth, multer, saucesCtrl.createProduct);
-router.put('/:id', auth, multer, saucesCtrl.modifyProduct);
-router.delete('/:id', auth, saucesCtrl.deleteProduct);
-router.get('/:id', auth, saucesCtrl.getOneProduct);
+router.post('/', auth, multer, saucesCtrl.createSauce);
+router.put('/:id', auth, multer, saucesCtrl.modifySauce);
+router.delete('/:id', auth, saucesCtrl.deleteSauce);
+router.get('/:id', auth, saucesCtrl.getOneSauce);
 router.get('/', auth, saucesCtrl.getAllSauces);
 
 module.exports = router;
