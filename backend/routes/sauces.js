1379f89bb786bbbfc7ebd0561ccacb218e1f97f1
Quentin BUXO <katouprog@gmail.com>
2021-11-20 18:10:20 +0900

    BACK - création de likedStatus

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -7,6 +7,7 @@ const multer = require('../middleware/multer-config');
 const saucesCtrl = require('../controllers/sauces');
 
 router.post('/', auth, multer, saucesCtrl.createSauce);
+router.post('/:id/like', auth, multer, saucesCtrl.likedStatus);
 router.put('/:id', auth, multer, saucesCtrl.modifySauce);
 router.delete('/:id', auth, saucesCtrl.deleteSauce);
 router.get('/:id', auth, saucesCtrl.getOneSauce);
