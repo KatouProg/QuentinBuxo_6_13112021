eef9a69151258dab28ac37e9ec19b98448fba42b
Quentin BUXO <katouprog@gmail.com>
2021-11-22 18:01:27 +0900

    BACK - Projet terminé_petites finitions

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -73,43 +73,53 @@ exports.getAllSauces = (req, res, next) => {
 
 
 // Like / Dislike une sauce
+
 exports.likeDislikeSauce = (req, res, next) => {
   let like = req.body.like
   let userId = req.body.userId
   let sauceId = req.params.id
-          // Sauce liked
-  switch (like) {
-    case 1 :
-        Sauce.updateOne({ _id: sauceId }, { $push: { usersLiked: userId }, $inc: { likes: +1 }})
-          .then(() => res.status(200).json({ message: `J'aime` }))
-          .catch((error) => res.status(400).json({ error }))
-            
-      break;
-          // Annulation du like / dislike
-    case 0 :
-        Sauce.findOne({ _id: sauceId })
-           .then((sauce) => {
-            if (sauce.usersLiked.includes(userId)) { 
-              Sauce.updateOne({ _id: sauceId }, { $pull: { usersLiked: userId }, $inc: { likes: -1 }})
-                .then(() => res.status(200).json({ message: `Neutre` }))
-                .catch((error) => res.status(400).json({ error }))
-            }
-            if (sauce.usersDisliked.includes(userId)) { 
-              Sauce.updateOne({ _id: sauceId }, { $pull: { usersDisliked: userId }, $inc: { dislikes: -1 }})
-                .then(() => res.status(200).json({ message: `Neutre` }))
-                .catch((error) => res.status(400).json({ error }))
-            }
-          })
-          .catch((error) => res.status(404).json({ error }))
-      break;
-          // Sauce Disliked et mise à jour avec des new valeurs
-    case -1 :
-        Sauce.updateOne({ _id: sauceId }, { $push: { usersDisliked: userId }, $inc: { dislikes: +1 }})
-          .then(() => { res.status(200).json({ message: `Je n'aime pas` }) })
-          .catch((error) => res.status(400).json({ error }))
-      break;
+          
+  
+// Sauce liked
+
+switch (like) {
+  case 1 :
+      Sauce.updateOne({ _id: sauceId }, { $push: { usersLiked: userId }, $inc: { likes: +1 }})
+        .then(() => res.status(200).json({ message: `J'aime` }))
+        .catch((error) => res.status(400).json({ error }))
+          
+  break;
+          
       
-      default:
-        console.log(error);
+// Annulation du like / dislike
+
+  case 0 :
+      Sauce.findOne({ _id: sauceId })
+          .then((sauce) => {
+          if (sauce.usersLiked.includes(userId)) { 
+            Sauce.updateOne({ _id: sauceId }, { $pull: { usersLiked: userId }, $inc: { likes: -1 }})
+              .then(() => res.status(200).json({ message: `Je ne like plus` }))
+              .catch((error) => res.status(400).json({ error }))
+          }
+          if (sauce.usersDisliked.includes(userId)) { 
+            Sauce.updateOne({ _id: sauceId }, { $pull: { usersDisliked: userId }, $inc: { dislikes: -1 }})
+              .then(() => res.status(200).json({ message: `Je ne dislike plus` }))
+              .catch((error) => res.status(400).json({ error }))
+          }
+        })
+        .catch((error) => res.status(404).json({ error }))
+  break;
+          
+      
+// Sauce Disliked et mise à jour avec des nouvelles valeurs
+
+  case -1 :
+      Sauce.updateOne({ _id: sauceId }, { $push: { usersDisliked: userId }, $inc: { dislikes: +1 }})
+        .then(() => { res.status(200).json({ message: `Je n'aime pas` }) })
+        .catch((error) => res.status(400).json({ error }))
+  break;
+  
+  default:
+    console.log(error);
   }
 }
