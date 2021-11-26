8d5e12f79917c0dec474fb7f98e94f173853d16b
Quentin BUXO <katouprog@gmail.com>
2021-11-26 22:01:38 +0900

    BACK - install dotenv + test (pas concluants --> essayer de trouver le token)

--- backend/app.js
+++ backend/app.js
@@ -1,34 +1,43 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const helmet = require("helmet");
-const mongoose = require('mongoose');
-const path = require('path');
+const mongoose = require("mongoose");
+const path = require("path");
+//require('dotenv').config();
 
-const saucesRoutes = require('./routes/sauces');
-const userRoutes = require('./routes/user');
+const saucesRoutes = require("./routes/sauces");
+const userRoutes = require("./routes/user");
 
-mongoose.connect('mongodb+srv://katouprog:Janjakus30@piiquanteocp6.75o3e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+mongoose
+  .connect(
+    "mongodb+srv://katouprog:Janjakus30@piiquanteocp6.75o3e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", //process.env.SECRET_DB
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 const app = express();
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 
 app.use(bodyParser.json());
 
 app.use(helmet());
 
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
-app.use('/api/sauces', saucesRoutes);
-app.use('/api/auth', userRoutes);
+app.use("/api/sauces", saucesRoutes);
+app.use("/api/auth", userRoutes);
 
 module.exports = app;
