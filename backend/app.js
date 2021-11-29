97d9aa998bf3acd874cea76de6045d4878bd3587
Quentin BUXO <katouprog@gmail.com>
2021-11-29 22:23:43 +0900

    BACK - Tests dotenv

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const userRoutes = require("./routes/user");
 
 mongoose
   .connect(
-    "mongodb+srv://katouprog:Janjakus30@piiquanteocp6.75o3e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", //process.env.SECRET_DB
+    "mongodb+srv://katouprog:Janjakus30@piiquanteocp6.75o3e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", // process.env.SECRET_DB
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => console.log("Connexion à MongoDB réussie !"))
