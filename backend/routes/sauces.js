2662107dae1c08c8b2cf8ead5895594750d57c47
Quentin BUXO <katouprog@gmail.com>
2021-11-30 13:19:54 +0900

    BACK - dotenv fonctionnel

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -1,16 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
+const saucesCtrl = require("../controllers/sauces");
 
-const auth = require('../middleware/auth');
-const multer = require('../middleware/multer-config');
-
-const saucesCtrl = require('../controllers/sauces');
-
-router.post('/', auth, multer, saucesCtrl.createSauce);
+router.post("/", auth, multer, saucesCtrl.createSauce);
 router.post("/:id/like", auth, saucesCtrl.likeDislikeSauce);
-router.put('/:id', auth, multer, saucesCtrl.modifySauce);
-router.delete('/:id', auth, saucesCtrl.deleteSauce);
-router.get('/:id', auth, saucesCtrl.getOneSauce);
-router.get('/', auth, saucesCtrl.getAllSauces);
+router.put("/:id", auth, multer, saucesCtrl.modifySauce);
+router.delete("/:id", auth, saucesCtrl.deleteSauce);
+router.get("/:id", auth, saucesCtrl.getOneSauce);
+router.get("/", auth, saucesCtrl.getAllSauces);
 
 module.exports = router;
